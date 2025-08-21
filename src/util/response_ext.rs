9d8cd3e931c4ea61c48c7282ab0ccab285a6654f
Abi أب <el@elabee.me>
2025-08-21 22:15:56 +0700

    response ext test

--- src/util/response_ext.rs
+++ src/util/response_ext.rs
@@ -12,3 +12,16 @@ impl<T> ResponseExt<T> for Response<T> {
     Ok(response)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::util::response_ext::ResponseExt;
+  use hyper::{Response, StatusCode};
+
+  #[test]
+  fn internal_server_error() {
+    let response = Response::internal_server_error("Foo");
+    assert!(response.is_ok());
+    assert_eq!(response.unwrap().status(), StatusCode::INTERNAL_SERVER_ERROR);
+  }
+}
