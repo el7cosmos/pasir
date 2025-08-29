eaffd2dca5070d81a5da868c83d010215e92a4d5
Abi أب <el@elabee.me>
2025-08-29 22:54:07 +0700

    github action workflow

--- patches/accel_find_sapi.php
+++ patches/accel_find_sapi.php
@@ -4,4 +4,4 @@ if (patch_point() !== 'after-php-extract') {
     return;
 }
 
-f_passthru(sprintf('patch -d source/php-src < %s/accel_find_sapi.patch', __DIR__));
+f_passthru(sprintf('patch -p1 -d %s/php-src < %s/accel_find_sapi.patch', SOURCE_PATH, __DIR__));
