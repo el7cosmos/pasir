6933fa69d504e1f841508d4ba3d0529dfd65a68e
Abi أب <el@elabee.me>
2025-10-18 16:36:19 +0700

    remove resolve_php_index and introduce parse_uri

--- src/sapi/context.rs
+++ src/sapi/context.rs
@@ -1,3 +1,6 @@
+use std::ffi::CString;
+use std::ffi::NulError;
+use std::ffi::c_int;
 use std::ffi::c_void;
 use std::net::SocketAddr;
 use std::path::Path;
@@ -6,6 +9,9 @@ use std::sync::Arc;
 
 use bytes::Bytes;
 use ext_php_rs::zend::SapiGlobals;
+use headers::ContentLength;
+use headers::ContentType;
+use headers::HeaderMapExt;
 use hyper::HeaderMap;
 use hyper::Request;
 use hyper::Response;
@@ -25,7 +31,6 @@ use tracing::instrument;
 use crate::cli::serve::Stream;
 use crate::sapi::ext::FromSapiHeaders;
 use crate::sapi::util::handle_abort_connection;
-use crate::service::php::PhpRoute;
 
 #[derive(Clone, Debug, Default)]
 pub(crate) enum ResponseType {
@@ -37,7 +42,8 @@ pub(crate) enum ResponseType {
 #[derive(Debug, Default)]
 pub(crate) struct Context {
   root: Arc<PathBuf>,
-  route: PhpRoute,
+  script_name: String,
+  path_info: Option<String>,
   stream: Arc<Stream>,
   request: Request<Bytes>,
   headers: HeaderMap,
@@ -48,13 +54,56 @@ pub(crate) struct Context {
 impl Context {
   pub(crate) fn new(
     root: Arc<PathBuf>,
-    route: PhpRoute,
     stream: Arc<Stream>,
     request: Request<Bytes>,
     sender: ContextSender,
   ) -> Self {
-    let headers = HeaderMap::new();
-    Self { root, route, stream, request, sender, headers, request_finished: false }
+    let uri = request.uri().path().to_string();
+    let mut context = Self {
+      root,
+      script_name: Default::default(),
+      path_info: None,
+      stream,
+      request,
+      sender,
+      headers: Default::default(),
+      request_finished: false,
+    };
+    context.parse_uri(uri, None);
+    context
+  }
+
+  fn parse_uri(&mut self, uri: String, path_info: Option<String>) {
+    let root = self.root.as_path();
+    let file = root.join(uri.trim_start_matches('/'));
+    self.path_info = path_info;
+
+    // If we are at the document root, route to `/index.php`.
+    if file == root {
+      self.script_name = "/index.php".to_string();
+      return;
+    }
+
+    if file.is_file() && uri.ends_with(".php") {
+      self.script_name = uri;
+      return;
+    }
+
+    if file.is_dir() {
+      let index = file.join("index.php");
+      if index.is_file() {
+        self.script_name = format!("{}/index.php", uri.trim_end_matches('/'));
+        return;
+      }
+    }
+
+    if let Some(name) = file.file_name() {
+      let suffix = format!("/{}", name.to_string_lossy());
+      let path_info = format!("{}{}", suffix, self.path_info.take().unwrap_or_default());
+      if let Some(uri) = uri.strip_suffix(suffix.as_str()) {
+        self.parse_uri(uri.to_string(), Some(path_info));
+      }
+    }
   }
 
   #[must_use = "losing the pointer will leak memory"]
@@ -76,8 +125,12 @@ impl Context {
     self.root.as_path()
   }
 
-  pub(crate) fn route(&self) -> &PhpRoute {
-    &self.route
+  pub(crate) fn script_name(&self) -> &str {
+    &self.script_name
+  }
+
+  pub(crate) fn path_info(&self) -> Option<&str> {
+    self.path_info.as_deref()
   }
 
   pub(crate) fn local_addr(&self) -> SocketAddr {
@@ -98,6 +151,52 @@ impl Context {
 
   pub(crate) fn body_mut(&mut self) -> &mut Bytes {
     self.request.body_mut()
+  }
+
+  #[instrument(skip(self), err)]
+  pub(crate) fn init_sapi_globals(&self) -> Result<(), NulError> {
+    let uri = self.request.uri();
+    let headers = self.request.headers();
+    let path_translated = format!("{}{}", self.root.to_string_lossy(), self.script_name);
+
+    let mut sapi_globals = SapiGlobals::get_mut();
+
+    sapi_globals.sapi_headers.http_response_code = StatusCode::OK.as_u16() as c_int;
+    sapi_globals.request_info.request_method =
+      CString::new(self.request.method().as_str())?.into_raw().cast_const();
+    sapi_globals.request_info.query_string = uri
+      .query()
+      .and_then(|query| CString::new(query).ok())
+      .map(|query| query.into_raw())
+      .unwrap_or_else(std::ptr::null_mut);
+    sapi_globals.request_info.path_translated = CString::new(path_translated)?.into_raw();
+    sapi_globals.request_info.request_uri = CString::new(uri.to_string())?.into_raw();
+    sapi_globals.request_info.content_length = headers
+      .typed_get::<ContentLength>()
+      .map_or(0, |content_length| content_length.0.cast_signed());
+    sapi_globals.request_info.content_type =
+      headers.typed_get::<ContentType>().map_or(Ok(std::ptr::null()), |content_type| {
+        CString::new(content_type.to_string())
+          .map(|content_type| content_type.into_raw().cast_const())
+      })?;
+
+    if let Some(auth) = headers.get("Authorization") {
+      unsafe {
+        pasir::ffi::php_handle_auth_data(CString::new(auth.as_bytes())?.as_ptr());
+      }
+    }
+
+    let proto_num = match self.request.version() {
+      Version::HTTP_09 => 900,
+      Version::HTTP_10 => 1000,
+      Version::HTTP_11 => 1100,
+      Version::HTTP_2 => 2000,
+      Version::HTTP_3 => 3000,
+      _ => unreachable!(),
+    };
+    sapi_globals.request_info.proto_num = proto_num;
+
+    Ok(())
   }
 
   pub(crate) fn append_response_header<K>(&mut self, key: K, value: HeaderValue)
@@ -173,8 +272,13 @@ impl ContextBuilder {
     self
   }
 
-  pub fn route(mut self, route: PhpRoute) -> Self {
-    self.0.route = route;
+  pub fn script_name(mut self, script_name: impl Into<String>) -> Self {
+    self.0.script_name = script_name.into();
+    self
+  }
+
+  pub fn path_info(mut self, path_info: impl Into<String>) -> Self {
+    self.0.path_info = Some(path_info.into());
     self
   }
 
@@ -226,15 +330,81 @@ impl ContextSender {
 
 #[cfg(test)]
 mod tests {
+  use std::ffi::c_int;
+  use std::path::PathBuf;
+
+  use bytes::Bytes;
   use ext_php_rs::zend::SapiGlobals;
+  use hyper::Method;
+  use hyper::Request;
+  use hyper::StatusCode;
+  use hyper::Uri;
+  use hyper::Version;
+  use hyper::header::CONTENT_LENGTH;
+  use hyper::header::CONTENT_TYPE;
+  use rstest::rstest;
 
   use crate::sapi::context::Context;
   use crate::sapi::context::ContextBuilder;
   use crate::sapi::context::ContextSender;
   use crate::sapi::tests::TestSapi;
 
+  #[rstest]
+  #[case("/", "/index.php", None)]
+  #[case("/foo", "/foo/index.php", None)]
+  #[case("/foo/", "/foo/index.php", None)]
+  #[case("/foo/bar/baz", "/foo/index.php", Some("/bar/baz"))]
+  #[case("/foo/index.php/bar", "/foo/index.php", Some("/bar"))]
+  #[case("/foo/foo.php", "/foo/foo.php", None)]
+  #[case("/foo/foo.php/bar", "/foo/foo.php", Some("/bar"))]
+  #[case("/bar/baz", "/index.php", Some("/bar/baz"))]
+  #[trace]
+  fn test_parse_uri(
+    #[case] request_uri: String,
+    #[case] script_name: &str,
+    #[case] path_info: Option<&str>,
+  ) {
+    let root = PathBuf::from("tests/fixtures/root");
+    let mut context = ContextBuilder::default().root(root).build();
+    context.parse_uri(request_uri, None);
+    assert_eq!(context.script_name(), script_name);
+    assert_eq!(context.path_info(), path_info);
+  }
+
   #[test]
-  fn test_context_flush() {
+  fn test_init_sapi_globals() {
+    let _guard = TestSapi::new();
+
+    let uri = Uri::builder().path_and_query("/foo?bar=baz").build().unwrap();
+    let request = Request::builder()
+      .method(Method::POST)
+      .version(Version::HTTP_3)
+      .header(CONTENT_LENGTH, "Foo Bar".len())
+      .header(CONTENT_TYPE, "text/plain")
+      .uri(uri)
+      .body(Bytes::default())
+      .unwrap();
+    let mut context = ContextBuilder::default().request(request).build();
+    context.script_name = "./index.php".to_string();
+    let results = context.init_sapi_globals();
+    assert!(results.is_ok());
+
+    let sapi_globals = SapiGlobals::get();
+    assert_eq!(sapi_globals.sapi_headers().http_response_code, StatusCode::OK.as_u16() as c_int);
+
+    let request_info = sapi_globals.request_info();
+    assert_eq!(request_info.request_method(), Some(Method::POST.as_str()));
+    assert_eq!(request_info.query_string(), Some("bar=baz"));
+    assert_eq!(request_info.path_translated(), Some("./index.php"));
+    assert_eq!(request_info.request_uri(), Some("/foo?bar=baz"));
+    assert_eq!(request_info.content_length(), "Foo Bar".len() as i64);
+    assert_eq!(request_info.content_type(), Some("text/plain"));
+    assert_eq!(request_info.auth_user(), None);
+    assert_eq!(request_info.proto_num(), 3000);
+  }
+
+  #[test]
+  fn test_flush() {
     let _sapi = TestSapi::new();
 
     let (_head_rx, _body_rx, context_sender) = ContextSender::receiver();
--- src/service/php.rs
+++ src/service/php.rs
@@ -1,9 +1,6 @@
 use std::convert::Infallible;
 use std::ffi::CString;
-use std::ffi::NulError;
 use std::ffi::c_void;
-use std::os::raw::c_int;
-use std::path::Path;
 use std::path::PathBuf;
 use std::pin::Pin;
 use std::sync::Arc;
@@ -13,19 +10,13 @@ use bytes::Bytes;
 use ext_php_rs::embed::Embed;
 use ext_php_rs::zend::SapiGlobals;
 use ext_php_rs::zend::try_catch_first;
-use headers::ContentLength;
-use headers::ContentType;
-use headers::HeaderMapExt;
 use http_body_util::BodyExt;
 use http_body_util::Empty;
 use http_body_util::Full;
 use http_body_util::combinators::UnsyncBoxBody;
 use hyper::Request;
 use hyper::Response;
-use hyper::StatusCode;
-use hyper::Version;
 use hyper::body::Incoming;
-use hyper::http::request::Parts;
 use pasir::error::PhpError;
 use pasir::ffi::ZEND_RESULT_CODE_FAILURE;
 use tower::Service;
@@ -58,7 +49,6 @@ impl Service<Request<Incoming>> for PhpService {
 
     Box::pin(async move {
       let (head, body) = req.into_parts();
-      let route = resolve_php_index(root.as_path(), head.uri.path());
       let bytes = match body.collect().await {
         Ok(collected) => collected.to_bytes(),
         Err(_) => return Response::internal_server_error(error_body),
@@ -73,13 +63,12 @@ impl Service<Request<Incoming>> for PhpService {
         unsafe { ext_php_rs::embed::ext_php_rs_sapi_per_thread_init() }
         unsafe { pasir::ffi::zend_update_current_locale() }
 
-        let path_translated = format!("{}{}", root.to_str().unwrap(), route.script_name());
-        if init_sapi_globals(&head, path_translated.as_str()).is_err() {
+        let request = Request::from_parts(head, bytes);
+        let context = Context::new(root, stream, request, context_tx);
+        if context.init_sapi_globals().is_err() {
           return error_tx.send(Response::bad_request);
         }
 
-        let request = Request::from_parts(head, bytes);
-        let context = Context::new(root, route, stream, request, context_tx);
         if let Err(e) = execute_php(context) {
           let callback = match e {
             PhpError::RequestStartupFailed => Response::service_unavailable,
@@ -110,73 +99,9 @@ impl Service<Request<Incoming>> for PhpService {
   }
 }
 
-/// Represents a resolved PHP route
-#[derive(Debug, Default, Clone, PartialEq)]
-pub struct PhpRoute {
-  script_name: String,
-  path_info: Option<String>,
-}
-
-impl PhpRoute {
-  pub(crate) fn new(script_name: String, path_info: Option<String>) -> Self {
-    Self { script_name, path_info }
-  }
-
-  pub(crate) fn script_name(&self) -> &str {
-    &self.script_name
-  }
-
-  pub(crate) fn path_info(&self) -> Option<&str> {
-    self.path_info.as_deref()
-  }
-}
-
-#[instrument(skip(head, path_translated), err)]
-fn init_sapi_globals(head: &Parts, path_translated: &str) -> Result<(), NulError> {
-  let mut sapi_globals = SapiGlobals::get_mut();
-  sapi_globals.sapi_headers.http_response_code = StatusCode::OK.as_u16() as c_int;
-  sapi_globals.request_info.request_method =
-    CString::new(head.method.as_str())?.into_raw().cast_const();
-  sapi_globals.request_info.query_string = head
-    .uri
-    .query()
-    .and_then(|query| CString::new(query).ok())
-    .map(|query| query.into_raw())
-    .unwrap_or_else(std::ptr::null_mut);
-  sapi_globals.request_info.path_translated = CString::new(path_translated)?.into_raw();
-  sapi_globals.request_info.request_uri = CString::new(head.uri.to_string())?.into_raw();
-  sapi_globals.request_info.content_length = head
-    .headers
-    .typed_get::<ContentLength>()
-    .map_or(0, |content_length| content_length.0.cast_signed());
-  sapi_globals.request_info.content_type =
-    head.headers.typed_get::<ContentType>().map_or(Ok(std::ptr::null()), |content_type| {
-      CString::new(content_type.to_string())
-        .map(|content_type| content_type.into_raw().cast_const())
-    })?;
-
-  if let Some(auth) = head.headers.get("Authorization") {
-    unsafe {
-      pasir::ffi::php_handle_auth_data(CString::new(auth.as_bytes())?.as_ptr());
-    }
-  }
-
-  let proto_num = match head.version {
-    Version::HTTP_09 => 900,
-    Version::HTTP_10 => 1000,
-    Version::HTTP_11 => 1100,
-    Version::HTTP_2 => 2000,
-    Version::HTTP_3 => 3000,
-    _ => unreachable!(),
-  };
-  sapi_globals.request_info.proto_num = proto_num;
-
-  Ok(())
-}
-
 #[instrument(skip(context), err)]
 fn execute_php(context: Context) -> Result<(), PhpError> {
-  let script = context.root().join(context.route().script_name().trim_start_matches("/"));
+  let script = context.root().join(context.script_name().trim_start_matches("/"));
 
   let context_raw = context.into_raw();
   SapiGlobals::get_mut().server_context = context_raw.cast::<c_void>();
@@ -208,176 +133,4 @@ fn request_shutdown() {
 
   let mut request_info = SapiGlobals::get().request_info;
   free_raw_cstring_mut!(request_info, path_translated);
-}
-
-/// Resolves a request URI to the appropriate PHP file and path info
-/// Returns PhpRoute with script_name and path_info
-fn resolve_php_index(document_root: &Path, request_uri: &str) -> PhpRoute {
-  // Clean the request URI (remove query string, normalize slashes)
-  let clean_uri = request_uri.split('?').next().unwrap_or(request_uri);
-  let clean_uri = clean_uri.trim_start_matches('/').trim_end_matches('/');
-
-  // Split the path into segments
-  let segments: Vec<&str> = if clean_uri.is_empty() {
-    vec![]
-  } else {
-    clean_uri.split('/').filter(|s| !s.is_empty()).collect()
-  };
-
-  // Check if the URI explicitly contains a .php file
-  if let Some(php_pos) = segments.iter().position(|&s| s.ends_with(".php")) {
-    // URI contains a PHP file explicitly
-    let script_path_segments = &segments[0..php_pos + 1]; // Include the .php file
-    let path_info_segments = &segments[php_pos + 1..];
-
-    // Build the script path
-    let script_path = format!("/{}", script_path_segments.join("/"));
-
-    // Build path info
-    let path_info = if path_info_segments.is_empty() {
-      None
-    } else {
-      Some(format!("/{}", path_info_segments.join("/")))
-    };
-
-    // Verify the PHP file actually exists
-    let mut file_path = document_root.to_path_buf();
-    for segment in script_path_segments {
-      file_path.push(segment);
-    }
-
-    if file_path.exists() && file_path.is_file() {
-      return PhpRoute::new(script_path, path_info);
-    }
-  }
-
-  // Try to find index.php by traversing from the most specific to the least specific path
-  for i in (0..=segments.len()).rev() {
-    let current_path_segments = &segments[0..i];
-    let remaining_segments = &segments[i..];
-
-    // Build the directory path to check
-    let mut dir_path = document_root.to_path_buf();
-    for segment in current_path_segments {
-      dir_path.push(segment);
-    }
-
-    // Check if index.php exists in this directory
-    let index_php_path = dir_path.join("index.php");
-    if index_php_path.exists() && index_php_path.is_file() {
-      // Found index.php, construct the paths
-      let relative_index_path = if current_path_segments.is_empty() {
-        "/index.php".to_string()
-      } else {
-        format!("/{}/index.php", current_path_segments.join("/"))
-      };
-
-      let path_info = if remaining_segments.is_empty() {
-        None
-      } else {
-        Some(format!("/{}", remaining_segments.join("/")))
-      };
-
-      return PhpRoute::new(relative_index_path, path_info);
-    }
-  }
-
-  // Fallback to root index.php if it exists
-  let root_index = document_root.join("index.php");
-  if root_index.exists() && root_index.is_file() {
-    let path_info = if clean_uri.is_empty() { None } else { Some(format!("/{clean_uri}")) };
-    PhpRoute::new("/index.php".to_string(), path_info)
-  } else {
-    // No index.php found anywhere
-    PhpRoute::new("/index.php".to_string(), Some(format!("/{clean_uri}")))
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use std::fs;
-
-  use hyper::Method;
-  use hyper::Uri;
-  use hyper::header::CONTENT_LENGTH;
-  use hyper::header::CONTENT_TYPE;
-
-  use super::*;
-  use crate::sapi::tests::TestSapi;
-
-  #[test]
-  fn test_init_sapi_globals() {
-    let _guard = TestSapi::new();
-
-    let uri = Uri::builder().path_and_query("/foo?bar=baz").build().unwrap();
-    let (head, _) = Request::builder()
-      .method(Method::POST)
-      .version(Version::HTTP_3)
-      .header(CONTENT_LENGTH, "Foo Bar".len())
-      .header(CONTENT_TYPE, "text/plain")
-      .uri(uri)
-      .body(Bytes::default())
-      .unwrap()
-      .into_parts();
-    let results = init_sapi_globals(&head, "./index.php");
-    assert!(results.is_ok());
-
-    let sapi_globals = SapiGlobals::get();
-    assert_eq!(sapi_globals.sapi_headers().http_response_code, StatusCode::OK.as_u16() as c_int);
-
-    let request_info = sapi_globals.request_info();
-    assert_eq!(request_info.request_method(), Some(Method::POST.as_str()));
-    assert_eq!(request_info.query_string(), Some("bar=baz"));
-    assert_eq!(request_info.path_translated(), Some("./index.php"));
-    assert_eq!(request_info.request_uri(), Some("/foo?bar=baz"));
-    assert_eq!(request_info.content_length(), "Foo Bar".len() as i64);
-    assert_eq!(request_info.content_type(), Some("text/plain"));
-    assert_eq!(request_info.auth_user(), None);
-    assert_eq!(request_info.proto_num(), 3000);
-  }
-
-  #[test]
-  fn test_resolve_php_index() {
-    // Create a temporary directory structure for testing
-    let temp_dir = std::env::temp_dir().join("php_test");
-    let _ = fs::remove_dir_all(&temp_dir); // Clean up if exists
-    fs::create_dir_all(&temp_dir).unwrap();
-    fs::create_dir_all(temp_dir.join("some_path")).unwrap();
-
-    // Create index.php files and a status.php file
-    fs::write(temp_dir.join("index.php"), "<?php echo 'root'; ?>").unwrap();
-    fs::write(temp_dir.join("some_path/index.php"), "<?php echo 'some_path'; ?>").unwrap();
-    fs::write(temp_dir.join("some_path/status.php"), "<?php echo 'status'; ?>").unwrap();
-
-    // Test cases
-    let test_cases = vec![
-      ("/", PhpRoute::new("/index.php".to_string(), None)),
-      (
-        "/some_path/admin",
-        PhpRoute::new("/some_path/index.php".to_string(), Some("/admin".to_string())),
-      ),
-      (
-        "/some_other_path/admin",
-        PhpRoute::new("/index.php".to_string(), Some("/some_other_path/admin".to_string())),
-      ),
-      ("/some_path/", PhpRoute::new("/some_path/index.php".to_string(), None)),
-      ("/some_path", PhpRoute::new("/some_path/index.php".to_string(), None)),
-      (
-        "/some_path/index.php/admin",
-        PhpRoute::new("/some_path/index.php".to_string(), Some("/admin".to_string())),
-      ),
-      (
-        "/some_path/status.php/admin",
-        PhpRoute::new("/some_path/status.php".to_string(), Some("/admin".to_string())),
-      ),
-    ];
-
-    for (uri, expected_route) in test_cases {
-      let route = resolve_php_index(&temp_dir, uri);
-      assert_eq!(route, expected_route, "Failed for URI: {}", uri);
-    }
-
-    // Clean up
-    let _ = fs::remove_dir_all(&temp_dir);
-  }
 }
