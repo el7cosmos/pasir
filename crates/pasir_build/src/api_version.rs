d58f10f362b1fe58a1365bee7113c6910d900468
Abi أب <el@elabee.me>
2026-02-14 12:13:35 +0700

    PHP 8.5 support (#66)
    * add php8.5 to pipeline
    
    * Updating ext-php-rs v0.15.1 -> v0.15.2
    
    * Add PHP 8.5 support
    
    * Disable coverage extension
    
    * ext-php-rs v0.15.2 -> v0.15.3
    
    * ext-php-rs v0.15.3 -> v0.15.6
    
    * add macos sdk path to clang arg

--- crates/pasir_build/src/api_version.rs
+++ crates/pasir_build/src/api_version.rs
@@ -6,11 +6,13 @@ use anyhow::anyhow;
 
 use crate::php_info::PHPInfo;
 
+#[allow(clippy::inconsistent_digit_grouping)]
 #[derive(PartialEq, PartialOrd)]
 enum ApiVersion {
-  Php82 = 20220829,
-  Php83 = 20230831,
-  Php84 = 20240924,
+  Php82 = 2022_08_29,
+  Php83 = 2023_08_31,
+  Php84 = 2024_09_24,
+  Php85 = 2025_09_25,
 }
 
 impl ApiVersion {
@@ -21,11 +23,11 @@ impl ApiVersion {
 
   /// Returns the maximum supported API version.
   pub const fn max() -> Self {
-    ApiVersion::Php84
+    ApiVersion::Php85
   }
 
   pub fn versions() -> Vec<Self> {
-    vec![ApiVersion::Php82, ApiVersion::Php83, ApiVersion::Php84]
+    vec![ApiVersion::Php82, ApiVersion::Php83, ApiVersion::Php84, ApiVersion::Php85]
   }
 
   /// Returns the API versions that are supported by this version.
@@ -38,6 +40,7 @@ impl ApiVersion {
       ApiVersion::Php82 => "php82",
       ApiVersion::Php83 => "php83",
       ApiVersion::Php84 => "php84",
+      ApiVersion::Php85 => "php85",
     }
   }
 }
@@ -49,7 +52,8 @@ impl TryFrom<u32> for ApiVersion {
     match version {
       x if ((ApiVersion::Php82 as u32)..(ApiVersion::Php83 as u32)).contains(&x) => Ok(ApiVersion::Php82),
       x if ((ApiVersion::Php83 as u32)..(ApiVersion::Php84 as u32)).contains(&x) => Ok(ApiVersion::Php83),
-      x if (ApiVersion::Php84 as u32) == x => Ok(ApiVersion::Php84),
+      x if ((ApiVersion::Php84 as u32)..(ApiVersion::Php85 as u32)).contains(&x) => Ok(ApiVersion::Php84),
+      x if (ApiVersion::Php85 as u32) == x => Ok(ApiVersion::Php85),
       version => Err(anyhow!(
         "The current version of PHP is not supported. Current PHP API version: {}, requires a version between {} and {}",
         version,
@@ -65,7 +69,7 @@ pub fn check_php_version(info: &PHPInfo) -> anyhow::Result<()> {
   let version = info.zend_version()?;
   let version: ApiVersion = version.try_into()?;
 
-  println!("cargo::rustc-check-cfg=cfg(php82, php83, php84)");
+  println!("cargo::rustc-check-cfg=cfg(php82, php83, php84, php85)");
   for supported_version in version.supported_apis() {
     println!("cargo:rustc-cfg={}", supported_version.cfg_name());
   }
